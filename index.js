6b631c7f5659321e049d7c154bf5b1637439ff0e
SRINU-NAYAK <srinukto@gmail.com>
2024-11-29 17:31:26 +0000

    super

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div className='super-over-league-background-container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div>
+      <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' className="images" />
+      <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' className="images" />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
